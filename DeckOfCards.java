04ffeaca838a087da895cf1ef91d2f54002898c2
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-23 18:42:30 +0530

    Contains main method

---
+++ DeckOfCards.java
@@ -0,0 +1,15 @@
+package com;
+
+public class DeckOfCards {
+    public static void main(String[] args) {
+        DistributeCards distributeCards = new DistributeCards();
+
+        for (int playerNumber = 1; playerNumber <= 4; playerNumber++) {
+            distributeCards.DistributeCards(playerNumber);
+            System.out.println("Player " + playerNumber + "'s cards:");
+            distributeCards.showCards(playerNumber);
+        }
+    }
+}
+
+
